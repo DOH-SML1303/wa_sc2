754841b022c0bf50567d9deb71f838fe95e437b5
Steph Lunn <stephanie.lundert@gmail.com>
2024-03-06 22:47:20 +0000

    Delete wa_scripts/test_scripts directory

--- wa_scripts/test_scripts/filter_sequences_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-input_file="$1"
-output_file="$2"
-pattern="USA/WA"
-
-xzcat "$input_file" | awk -v pattern="$pattern" 'BEGIN { RS=">"; ORS="" } $0 ~ pattern { print ">"$0 }' > "$output_file"
-
-echo "WA seqs have been filtered and written to $output_file."
